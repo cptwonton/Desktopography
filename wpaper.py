b0f659f0a77f49a04c2a43d544d2b02e2333042d
jayfurrie <jayfurrie@gmail.com>
2015-04-20 13:40:13 -0500

    almost done
    images aren't the right size

--- wpaper.py
+++ wpaper.py
@@ -2,6 +2,7 @@ import bs4
 import requests
 import re
 from urllib.parse import urljoin
+import os
 
 #list of url's to images
 urllist = []
@@ -24,16 +25,38 @@ partial_links = set(partial_links)
 
 #appends partial to base
 partial2_links = [urljoin(baseurl,l) for l in partial_links]
+partial2_links.sort()
 
 
+#creates directories to save wallpapers
+if(not os.path.exists(r"C:/Desktopography")):
+	os.makedirs(r"C:/Desktopography")
+for links in partial2_links:
+	currentyear = links[37:]
+	yeardir = r"C:/Desktopography/" + currentyear
+	if(not os.path.exists(yeardir)):
+		os.makedirs(yeardir)
+	r = requests.get(links)
+	soup = bs4.BeautifulSoup(r.text)
+	image_links = [links.get('href') for links in soup.find_all('a')]
+	image2_links = [l for l in image_links if l and currentyear in l and len(l.split(r'/'))==4]
+	for i in image2_links:
+		name = i[16:]
+		with open(yeardir+name+'.jpg', 'wb') as handle:
+			response = requests.get(links+name+'/1920x1200/download', stream=True)
+			for block in response.iter_content(1024):
+				if not block:
+					break
+			handle.write(block)
+		
+	
 
 
+	
+		
 
-#belongs in for loop, writes each wallpaper to file with unique name
-with open(name+'.jpg', 'wb') as handle:
-	response = requests.get(baseurl+indexurl+name+'/1920x1200/download', stream=True)
-	for block in response.iter_content(1024):
-		if not block:
-			break
-		handle.write(block)
+		
+	
+
+
 		
